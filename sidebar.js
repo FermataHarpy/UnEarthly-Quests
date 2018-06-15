f1fba5caccd4dd0a0f2a8ec49f93d7ac03a84e32
saralynn.dobler <saralynn.dobler@ddobler.com>
2018-06-15 14:43:18 -0500

    initial commit

---
+++ sidebar.js
@@ -0,0 +1,50 @@
+ document.getElementById("navMenu").innerHTML =
+ '<head> <link rel="shortcut icon" href="favicon.ico"> </head>'+
+ '<div id="header">'+
+  '              <div id="logo">'+
+   '                 <a href="index.html"><img alt="" src="images/ueqlogo.png" style="width: 200px;" /></a>'+
+    '                 <br><br>'+
+		  	                  '<input type="text" name="search" placeholder="Search..">'+
+     '           </div>'+
+      '          <div id="menu">'+
+       '             <ul>'+
+
+        '                <li class="current_page_item"><a title="" accesskey="1" href="index.html" class="home">Homepage</a>'+
+         '               </li>'+
+          '              <li><a title="" accesskey="2" href="Quests.html" class="quests">Quests</a>'+
+		 						 '<ul class="drpdwn-quests">'+
+		  							'<li><a href="quests.html#21q">21.0 Quests</a></li>'+
+		 							'<li><a href="quests.html#21q">1.0 Quests</a></li>'+
+		 						 '</ul>'+
+           '             </li>'+
+            '            <li><a title="" accesskey="3" href="BoardQuests.html" class="bquests">Board Quests</a>'+
+								 '<ul class="drpdwn-bquests">'+
+		  							'<li><a href="#">21.0 Board Quests</a></li>'+
+		 							'<li><a href="#">1.0 Board Quests</a></li>'+
+		 						 '</ul>'+
+             '           </li>'+
+              '          <li><a title="" accesskey="4" href="CardQuests.html" class="cquests">Card Quests</a>'+
+			  					 '<ul class="drpdwn-cquests">'+
+		  							'<li><a href="cquests21.html">21.0 Card Quests</a></li>'+
+		 							'<li><a href="#">1.0 Card Quests</a></li>'+
+		 						 '</ul>'+
+               '         </li>'+
+                '        <li><a title="" accesskey="5" href="PartyQuests.html" class="pquests">Party Quests</a>'+
+                 '       </li>'+
+                  '      <li><a title="" accesskey="6" href="About.html" class="aboutuq">About UnEarthly Quests</a>'+
+                   '     </li>'+
+                    '    <li><a title="" accesskey="5" href="Contact.html" class="contactuq">Contact</a>'+
+
+							 '<ul class="drpdwn-contact">'+
+							 	 '  <li><a title="" accesskey="5" href="Contact.html" class="ideas">Contact</a></li>'+
+                     			 '  <li><a title="" accesskey="5" href="Ideas.html" class="ideas">Ideas</a></li>'+
+                      			  '<li><a title="" accesskey="5" href="Donate.html" class="donate" >Donate</a></li>'+
+							'</ul>'+
+						'   </li>'+
+                    '</ul>'+
+                '</div>'+
+				'<div id="footer_sd">'+
+				'<p> Website & Games Fabricated by<br> <a href="https://www.slynnsdesigns.com">LadySlynn</a> </p>'+
+				'</div>'+				
+            '</div>';
+			
